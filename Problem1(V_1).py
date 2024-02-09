4c7ec0e9658d507ad345f34cd01a2d2789e4ef36
Pratik Walunj <142927254+HumankindX@users.noreply.github.com>
2024-02-09 19:38:16 +0530

    Create Problem1(V_1).py

---
+++ Problem1(V_1).py
@@ -0,0 +1,13 @@
+def if_else(n):
+    if n % 2 != 0:
+        return "Weird"
+    elif n % 2 == 0 and n > 2 and n < 5:
+        return "Not Weird"
+    elif n % 2 == 0 and n > 6 and n < 20:
+        return "Weird"
+    elif n % 2 == 0 and n > 20:
+        return "Not Weird"
+
+n = int(input())
+result = if_else(n)
+print(result)
